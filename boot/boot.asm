39206d745bc203555afa105419d552deb79d9e88
Quentin Patinier <quentin.patinier@gmail.com>
2023-01-10 11:27:40 +0800

    added disk operation

--- boot/boot.asm
+++ boot/boot.asm
@@ -8,37 +8,65 @@ org 0x7c00
 ; Bootsector
 bootsector :
 
-bootsector_init :
-	jmp 0x0000:bootsector_code
+	bootsector_init :
+		jmp 0x0000:bootsector_code
 
-bootsector_include :
-	%include "boot/print.asm"
-	%include "boot/screen.asm"
+	bootsector_include :
+		%include "boot/print.asm"
+		%include "boot/screen.asm"
+		%include "boot/disk.asm"
 
-bootsector_code :
+	bootsector_data :
+		NUM_SECTORS equ 1
+		START_SECTOR equ 2
 
-	; Prepare stack
-	mov sp, bootsector
+		DRIVE : db 0
+		mess : db "Quints OS", 0x0a, 0xd, 0
+		
+	bootsector_code :
 
-	; Set background color
-	mov bl, 0x09
-	call screen_background
+		; Initialise
+		mov [DRIVE], dl
+		mov sp, bootsector
 
-	; Print message
-	mov si, mess
-	call print
+		; Set background color
+		mov bl, 0x09
+		call screen_background
 
-	mov dx, 0xabcd
-	call hex_print
+		; Print message
+		mov si, mess
+		call print
 
-	; Hold
-	jmp $
+		; Load bootloader stage 2
+		xor ax, ax
+		mov es, ax
+		mov bx, bootloader_stage2
+
+		mov al, NUM_SECTORS
+		mov cl, START_SECTOR
+		mov dl, [DRIVE]
+		call read_disk
+
+		jmp bootloader_stage2_code
+
+		; Hold
+		hlt
 
 
-bootsector_data :
-	mess : db "Quints OS", 0x0a, 0xd, 0
 
 
-bootsector_magic :
-	times 510 - ($ - $$) db 0
-	dw 0xaa55
+	bootsector_magic :
+		times 510 - ($ - $$) db 0
+		dw 0xaa55
+
+bootloader_stage2 :
+	bootloader_stage2_data :
+		MESS1 : db "Hi there! Onto stage 2!", 0x0a, 0x0d, 0
+	bootloader_stage2_code :
+		mov si, MESS1
+		call print
+
+		hlt
+
+	bootloader_stage2_finish :
+		times 512 - ($ - bootloader_stage2) db 0
---
+++ boot/disk.asm
@@ -0,0 +1,57 @@
+;;;
+;;; Setting up disk operations in 16-bit mode
+;;;
+
+;;; 
+;;; Read from disk
+;;; 
+;;; Example :
+;;;     ; Prepare location
+;;;     mov ax, DATA_SEGMENT
+;;; 	mov es, ax
+;;;     mov bx, DATA_LOCATION
+;;;     
+;;;     ; Prepare reading info
+;;;     mov al, NUM_SECTORS
+;;;     mov cl, START_SECTOR
+;;;     mov dl, [DRIVE]
+;;; 	call read_disk
+;;; 
+;;; 	DRIVE : db 0x80
+;;; 
+
+read_disk :
+
+    pusha
+    ; Prepare read disk
+    mov ah, 0x02
+    mov ch, 0
+	mov dh, 0
+
+
+    push ax
+    int 0x13
+
+    ; Check error
+    jc read_disk_error
+
+    ; Check if number of sectors transfered is correct
+    mov bl, al
+
+    pop ax
+    cmp al, bl
+
+
+    jne read_disk_error
+
+    popa 
+    ret
+
+
+    read_disk_error :
+        mov si, DISK_ERROR_MESS
+        call print
+        hlt
+
+    DISK_ERROR_MESS : db "Error reading disk", 0
+
